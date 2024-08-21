0c16715f5cdcc10b69f96f8db185140138c7cc1b
mario-e-lopez-c <marioelopezc@gmail.com>
2024-08-20 23:53:09 -0500

    se agrega la tarea 8

--- React_Project/vite-project/src/components/Home.jsx
+++ React_Project/vite-project/src/components/Home.jsx
@@ -34,6 +34,12 @@ const Home = () => {
             justifyContent: 'center',
             flexWrap: 'wrap'
         },
+        search: {
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            marginTop:'7px'
+        },
         pagination: {
             display:'flex',
             justifyContent: 'center',
@@ -42,39 +48,80 @@ const Home = () => {
     }
 
     const [id, setId] = useState("");
-    // const [totalItems, setTotalItems] = useState("");
-    // const [itemsPerPage, setItemsPerPage] = useState("");
+    const [totalItems, setTotalItems] = useState("");
+    const [itemsPerPage, setItemsPerPage] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
-    const ids = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    const totalItems = ids.length;
-    const itemsPerPage = 7;
+    const [characters, setCharacters] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [query, setQuery] = useState('');
+    // const ids = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+    const [characterList, setCharactersList] = useState();
 
     useEffect( () => {
-        fetch("https://rickandmortyapi.com/api/character/")
-        .then((response) => response.json())
-        .then((result) => {
-            console.log('This is all result: ',result.results);
-            const idsArray = []
-            result.results.map((objeto) => (
-                idsArray.push(objeto.id)
-            ))
-            console.log('Este es idsArray: ',idsArray)
-          setId(idsArray);
-        // setTotalItems(totalItems);
-        // setItemsPerPage(itemsPerPage);
-        })
-    })
+        const fetchAllCharacters = async () => {
+            try {
+                const response = await fetch("https://rickandmortyapi.com/api/character/")
+                const data = await response.json();
+                    const idsArray = []
+                    data.results.map((objeto) => (
+                        idsArray.push(objeto.id)
+                    ))
+                    const indexOfLastImage = currentPage * itemsPerPage;
+                    const indexOfFirstImage = indexOfLastImage - itemsPerPage;
+                    const currentImages = characters.slice(indexOfFirstImage, indexOfLastImage);
+            //    <     console.log('Este es idsArray: ',idsArray)
+            //       setId(idsArray);>
+                     setCharacters(currentImages)
+                     setTotalItems(idsArray.length)
+                     setItemsPerPage(7)
+                } catch (error) {
+                console.error('Error fetching characters:', error);
+                }
+        }; 
+        fetchAllCharacters();
+       
+    }, [])
     // console.log('Este es idsArray afuera: ',id)
-    const indexOfLastImage = currentPage * itemsPerPage;
-    const indexOfFirstImage = indexOfLastImage - itemsPerPage;
-    const currentImages = ids.slice(indexOfFirstImage, indexOfLastImage);
+      // const totalItems = characters.length;
+    // const itemsPerPage = 7;
     
-//Calcular los indices de la primera y la ultima imagen a mostrar
-
-    console.log('This is current images: ',currentImages)
+    // setCharacters(currentImages)
+    //Calcular los indices de la primera y la ultima imagen a mostrar
 
     const handlePageChange = (page) => {
         setCurrentPage(page)
+    }
+
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            if(query.trim() === '') return; //no hacer solicitud si el término de búsqueda está vacío
+            try {
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`);
+            const data = await response.json();
+            const charactersArray = [];
+            data.results.map((objeto) => (
+                charactersArray.push(objeto.id)
+            ))
+            
+            const itemsPerPage = 7;
+            const indexOfLastImage = currentPage * itemsPerPage;
+            const indexOfFirstImage = indexOfLastImage - itemsPerPage;
+            const currentImages = charactersArray.slice(indexOfFirstImage, indexOfLastImage);
+            setCharacters(currentImages);
+            console.log('This is data from button: ', charactersArray)
+            setTotalItems(totalItems)
+            setItemsPerPage(itemsPerPage)
+            } catch (error) {
+            console.error('Error fetching characters:', error);
+            }
+        };
+    
+        fetchCharacters();
+        }, [query]);
+
+    const handleSearch = () => {
+        setQuery(searchTerm)
     }
 
     return(
@@ -87,8 +134,18 @@ const Home = () => {
                 <NavLink to="/about" style={styles.link} activeStyle={styles.activeLink}>Episodes</NavLink>
                 <NavLink to="/contact" style={styles.link} activeStyle={styles.activeLink}>About</NavLink>
             </nav>
+            <div style={styles.search}>
+                <input
+                    type="text"
+                    placeholder="Search images..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{marginRight: '10px'}}
+                />
+                <button onClick={handleSearch}>Search</button>
+            </div>
             <div style={styles.card}>
-                {currentImages.map((i) =>(
+                {characters.map((i) =>(
                     <RickAndMortyCharacterCard id={i}/>
                 ))}
             </div>
